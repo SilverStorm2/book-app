8468925d2ef9f8959f1054e2ce110513ea20b3eb
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2025-01-10 10:57:06 +0100

    Add BooksList class v2 (error fixed)

--- src/js/script.js
+++ src/js/script.js
@@ -9,19 +9,19 @@ class BooksList {
     this.render();
     this.initAction();  
   }
-  
+    
   getElements() {
     this.booksList = document.querySelector('.books-list');
     this.filtersForm = document.querySelector('.filters');
   }
-  
+    
   render(){
     this.booksList.innerHTML = '';
-  
+    
     for (const book of this.data) {
       const ratingWidth = book.rating * 10;
       const ratingBgc = this.determineRatingBgc(book.rating);
-  
+    
       const bookData = {
         id: book.id,
         name: book.name,
@@ -38,22 +38,22 @@ class BooksList {
     }
     this.filterBooks();
   }
-  
+    
   filterBooks() {
     for (const book of this.data) {
       let shouldBeHidden = false;
-    
+      
       for (const filter of this.filters) {
         if (!book.details[filter]) {
           shouldBeHidden = true;
           break;
         }
       }
-    
+      
       const bookElement = document.querySelector(
         `.book__image[data-id="${book.id}"]`
       );
-    
+      
       if (bookElement) {
         if (shouldBeHidden) {
           bookElement.classList.add('hidden');
@@ -63,5 +63,41 @@ class BooksList {
       }
     }
   }
-  
+  determineRatingBgc(rating) {
+    if (rating < 6) {
+      return 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if (rating > 9) {
+      return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+  }
+
+  initActions() {
+    this.filtersForm.addEventListener('click', (event) => {
+      if (
+        event.target.tagName === 'INPUT' &&
+        event.target.type === 'checkbox' &&
+        event.target.name === 'filter'
+      ) {
+        const filterValue = event.target.value;
+
+        if (event.target.checked && !this.filters.includes(filterValue)) {
+          this.filters.push(filterValue);
+        } else if (
+          !event.target.checked &&
+          this.filters.includes(filterValue)
+        ) {
+          const index = this.filters.indexOf(filterValue);
+          this.filters.splice(index, 1);
+        }
+        console.log('Current Filters:', this.filters);
+        this.filterBooks();
+      }
+    });
+  }
+    
 }
+new BooksList();
