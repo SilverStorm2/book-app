964785ffaed6071e8dda0d6a9fc019fdee911577
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2025-12-30 04:11:37 +0100

    added: 15 more books kept on imgbb web

--- src/js/data.js
+++ src/js/data.js
@@ -56,4 +56,169 @@ dataSource.books = [
       nonFiction: false
     }
   },
+  {
+    id: 6,
+    name: 'The Pragmatic Programmer',
+    image: 'https://i.ibb.co/WNX08Yt0/15-The-Pragmatic-Programmer.png',
+    rating: 9.1,
+    price: 45,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 7,
+    name: 'Clean Code',
+    image: 'https://i.ibb.co/n8K8qm6k/13-clean-code.png',
+    rating: 8.9,
+    price: 50,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 8,
+    name: 'The Great Gatsby',
+    image: 'https://i.ibb.co/sp8r84rv/5-the-great-gatsby.png',
+    rating: 7.9,
+    price: 22,
+    details: {
+      adults: false,
+      nonFiction: false
+    }
+  },
+  {
+    id: 9,
+    name: '1984',
+    image: 'https://i.ibb.co/Z172Bj38/12-1984.png',
+    rating: 8.7,
+    price: 18,
+    details: {
+      adults: true,
+      nonFiction: false
+    }
+  },
+  {
+    id: 10,
+    name: 'Brave New World',
+    image: 'https://i.ibb.co/4wKRbfwd/6-brave-new-world.png',
+    rating: 8.1,
+    price: 19,
+    details: {
+      adults: true,
+      nonFiction: false
+    }
+  },
+  {
+    id: 11,
+    name: 'To Kill a Mockingbird',
+    image: 'https://i.ibb.co/LdNsBD9h/9-to-Kill-a-Mockingbird.png',
+    rating: 8.4,
+    price: 24,
+    details: {
+      adults: false,
+      nonFiction: false
+    }
+  },
+  {
+    id: 12,
+    name: 'The Catcher in the Rye',
+    image: 'https://i.ibb.co/s98WPtcG/1-the-cather-in-the-rey.png',
+    rating: 7.2,
+    price: 17,
+    details: {
+      adults: true,
+      nonFiction: false
+    }
+  },
+  {
+    id: 13,
+    name: 'The Hobbit',
+    image: 'https://i.ibb.co/9mfyQRnh/11-the-hobbit.png',
+    rating: 8.6,
+    price: 28,
+    details: {
+      adults: false,
+      nonFiction: false
+    }
+  },
+  {
+    id: 14,
+    name: 'The Lord of the Rings',
+    image: 'https://i.ibb.co/cXJy1xCp/14-The-Lord-of-the-Rings.png',
+    rating: 9.0,
+    price: 60,
+    details: {
+      adults: false,
+      nonFiction: false
+    }
+  },
+  {
+    id: 15,
+    name: 'Sapiens',
+    image: 'https://i.ibb.co/cKLjFXVz/8-sapiens.png',
+    rating: 8.3,
+    price: 35,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 16,
+    name: 'Educated',
+    image: 'https://i.ibb.co/HDFLgZqf/3-educated.png',
+    rating: 7.6,
+    price: 26,
+    details: {
+      adults: true,
+      nonFiction: true
+    }
+  },
+  {
+    id: 17,
+    name: 'Thinking, Fast and Slow',
+    image: 'https://i.ibb.co/5hgQr10p/7-Thinking-Fast-and-Slow.png',
+    rating: 8.2,
+    price: 32,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 18,
+    name: 'Dune',
+    image: 'https://i.ibb.co/G4t1yYMJ/10-dune.png',
+    rating: 8.5,
+    price: 27,
+    details: {
+      adults: false,
+      nonFiction: false
+    }
+  },
+  {
+    id: 19,
+    name: 'The Road',
+    image: 'https://i.ibb.co/1J9GT2mn/2-the-road.png',
+    rating: 7.4,
+    price: 21,
+    details: {
+      adults: true,
+      nonFiction: false
+    }
+  },
+  {
+    id: 20,
+    name: 'A Brief History of Time',
+    image: 'https://i.ibb.co/nqz2qp5n/4-a-brief-history-of-time.png',
+    rating: 7.8,
+    price: 29,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
 ];
